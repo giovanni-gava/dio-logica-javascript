b6442916db115fa9c9a329b6b609ef671bebd1ff
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-19 19:22:55 -0300

    feat(js): add if

---
+++ dio-logica-js/estruturas-de-controle/IF/exemplos/if.js
@@ -0,0 +1,6 @@
+//IF consiste em uma estrutura de controle que permite avaliar uma expressão e, de acordo com seu resultado, executar uma determinada ação.
+let turnOn = true
+
+if (turnOn) {
+  console.log("Ligado")
+}
