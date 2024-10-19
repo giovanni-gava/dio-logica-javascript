fddd7b5bcaaa690a7a80565ffde7f42e67bae778
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-19 20:33:43 -0300

    feat(js): add if

---
+++ dio-logica-js/estruturas-de-controle/IF/exemplos/ifelse.js
@@ -0,0 +1,15 @@
+//Else if é utilizado para verificar mais de uma condição
+
+let hungryLevel = 4;
+
+if (hungryLevel === 0) {
+  console.log("Não está com fome");
+}
+
+else if (hungryLevel === 1) {
+  console.log("Está com fome");
+}
+
+else { 
+  console.log("Está com muita fome");
+}
