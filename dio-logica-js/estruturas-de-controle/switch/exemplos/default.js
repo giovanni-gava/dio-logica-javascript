ec59d242bbc7d86d7da92994127a7f5d297ba076
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-21 20:51:52 -0300

    feat(js): add switch

---
+++ dio-logica-js/estruturas-de-controle/switch/exemplos/default.js
@@ -0,0 +1,20 @@
+//Default é executado quando nenhum case é satisfeito
+
+let fruit = "lemon";
+
+switch(fruit) {
+    case "laranja":
+    console.log("Suco de laranja");
+    break;
+
+    case "banana":
+    console.log("Vitamina de banana");
+    break;
+
+    case "mamao":
+    console.log("Vitamina de mamão");
+    break;
+    
+    default:
+    console.log("Sem frutas");
+} 
---
+++ dio-logica-js/estruturas-de-controle/switch/exemplos/switch.js
@@ -0,0 +1,18 @@
+// Estrutura de controle de fluxo Switch
+// Switch é uma estrutura de controle de fluxo que permite que o programa tome decisões com base no valor de uma expressão.
+
+
+let fruit = "laranja";
+
+switch(fruit) {
+    case "laranja":
+    console.log("Suco de laranja");
+    break 
+
+    case "banana":
+    console.log("Vitamina de banana");
+    break;
+    
+    case "mamao":
+    console.log("Vitamina de mamão");
+} 
