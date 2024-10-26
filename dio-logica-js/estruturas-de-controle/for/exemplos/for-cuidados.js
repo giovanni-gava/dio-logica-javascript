e84a4784cda186d48ef52b4ad275100c2733c009
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-26 20:09:19 -0300

    feat(js): add for exemplos

---
+++ dio-logica-js/estruturas-de-controle/for/exemplos/for-cuidados.js
@@ -0,0 +1,9 @@
+
+let lifePoints = 0;
+
+for(let i = 0; i < 11 ; i++) {
+    lifePoints += 1;
+    console.log(`Ganhou 10 pontos de vida. Total: ${lifePoints}`);
+}
+
+console.log(lifePoints + " total")
---
+++ dio-logica-js/estruturas-de-controle/for/exemplos/for.js
@@ -0,0 +1,13 @@
+// For é utilizado para repetir um bloco de código várias vezes
+
+for (let contador = 0; contador < 6; contador++) { 
+    console.log(contador);
+    console.log("Aumentando o contador")
+}
+
+
+// O for é composto por 3 partes: 
+// a declaração da variável, a condição de parada e o incremento
+// Esse for está declarando a variável contador com valor 0
+// A condição de parada é que o contador seja menor que 4
+// E o incremento é que o contador seja incrementado em 1 a cada iteração
