06ee3d115218558388cdf4f50a8567e8f96db605
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-26 20:33:04 -0300

    feat(js): add for exemplos

---
+++ dio-logica-js/estruturas-de-controle/for/exemplos/do-while.js
@@ -0,0 +1,9 @@
+//Do while é uma estrutura de repetição que primeiro executa o bloco de código e depois verifica a condição.
+
+
+let contador = 0
+do {
+    console.log("Olá, mundo!")
+    contador++
+} while (contador < 3)
+    
