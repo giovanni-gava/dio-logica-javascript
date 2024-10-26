a9769ea7e0627bf9d974191d6ca905b5a0449c58
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-26 20:31:27 -0300

    feat(js): add for exemplos

---
+++ dio-logica-js/estruturas-de-controle/for/exemplos/while.js
@@ -0,0 +1,8 @@
+// While é uma estrutura de repetição que é executada enquanto uma condição é verdadeira ou falsa
+
+let contador = 0;
+
+while ( contador < 3 ){
+    console.log(contador + " contagem");
+    contador++;
+}
