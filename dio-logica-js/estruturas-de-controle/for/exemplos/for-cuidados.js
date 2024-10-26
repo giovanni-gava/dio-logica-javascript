de5e98cbd6d723dac3313de500ea09d5d3cd556a
Giovanni Gava <giovanni.aalvesdasilva@gmail.com>
2024-10-26 20:10:11 -0300

    feat(js): add for exemplos

--- dio-logica-js/estruturas-de-controle/for/exemplos/for-cuidados.js
+++ dio-logica-js/estruturas-de-controle/for/exemplos/for-cuidados.js
@@ -5,5 +5,5 @@ for(let i = 0; i < 11 ; i++) {
     lifePoints += 1;
     console.log(`Ganhou 10 pontos de vida. Total: ${lifePoints}`);
 }
-
+// Esse for está declarando a variável i com valor 0 e devemos incrementar 1 a cada iteração e prestar atencao pois a contagem comeca em 0
 console.log(lifePoints + " total")
